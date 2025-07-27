3a2c638f96d1a1e996d330827642fefe06178868
KheirEddine <bimusm3@gmail.com>
2025-07-26 18:11:48 -0700

    initial setup

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+@export var SPEED = 80
+@onready var animations = $AnimationPlayer
+
+func handle_movment():
+	var moveDirection = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = moveDirection * SPEED
+
+func update_Animations():
+	var direction = "Down"
+	
+	if velocity.x == 0:
+		if animations.is_playing():
+			animations.stop()
+	else:
+		if velocity.x < 0:
+			direction = "Left"
+		elif velocity.x > 0:
+			direction = "Right"
+		elif velocity.y < 0:
+			direction = "Up"
+		
+		animations.play("Idle_" + direction)
+
+func _physics_process(delta: float) -> void:
+	handle_movment()
+	update_Animations()
+	move_and_slide()
