7605b561438e78d90f62581283e42dd63c8e187d
KheirEddine <bimusm3@gmail.com>
2025-07-27 10:35:01 -0700

    Added necessary functions

--- Scripts/Components/health_component.gd
+++ Scripts/Components/health_component.gd
@@ -10,15 +10,20 @@ var is_dead : bool = false
 var is_low : bool = false
 
 func _ready() -> void:
+	Entity = get_parent()
 	current_health = max_health
+func _process(delta: float) -> void:
 	if current_health <= 0 :
 		is_dead = true
 	if current_health <= max_health / 4 :
 		is_low = true
-
 func take_damage(amount : float):
-	pass
+	if !is_dead:
+		current_health -= amount
 func add_health(amount : float ):
-	pass
+	if !is_dead and current_health <= max_health:
+		if current_health + amount > max_health:
+			current_health = max_health
+		
 func regenerate_health(amount : float):
 	pass
